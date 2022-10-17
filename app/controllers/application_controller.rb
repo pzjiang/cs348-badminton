f451b477694b90464525fa7901b10f0a37078c49
pzjiang <pzjiang01@hotmail.com>
2022-10-16 23:51:48 -0400

    devise set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_filter :require_login
+    # add to login:
+    # skip_before_filter :require_login
+    private
+
+    def require_login
+        unless current_user
+            redirect_to login_url
+    end
+  end
+
 end

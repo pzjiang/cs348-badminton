e8b8bafd52bd2078830bfedae83e55782b0bd340
pzjiang <pzjiang01@hotmail.com>
2022-10-16 23:41:50 -0400

    devise added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_032934) do
+ActiveRecord::Schema.define(version: 2022_10_17_034059) do
 
   create_table "challenges", force: :cascade do |t|
     t.integer "challenger_id"
@@ -87,6 +87,18 @@ ActiveRecord::Schema.define(version: 2022_10_17_032934) do
     t.integer "numbered"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

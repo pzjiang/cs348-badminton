442a1f5bc322ddf3baba0123b6afeffa1050ae91
root <root@LAPTOP-2C7VJDRN.localdomain>
2022-10-20 01:49:15 -0400

    Finished controller for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,20 +18,41 @@ class GamesController < ApplicationController
             # On successful save operation, redirect to the new game's page
             redirect_to @game
         else
+            # On a failure, redirect to new
             render :new, status: :unprocessable_entity
         end
     end
-    
+
     # Edit an existing game
+    def edit
+        @game = Game.find(params[:id])
+    end
+    
+    # Update an existing game (from games#edit)
     def update
+        @game = Game.find(params[:id])
+
+        if @article.save
+            # On successful update operation, redirect to the new game's page
+            redirect_to @game
+        else
+            # On a failure, redirect to edit
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     # Display a game
     def show
+        @game = Game.find(params[:id])
     end
 
     # Delete an existing game
     def destroy
+        @game = Game.find(params[:id])
+        @game.destroy
+
+        # Redirect to the games index
+        redirect_to :index
     end
 
     # Enforce integrity of game input data

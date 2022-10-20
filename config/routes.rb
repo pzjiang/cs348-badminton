46e440a0c5b2ef2d3d9e302d2e096c9f676f7b4e
root <root@LAPTOP-2C7VJDRN.localdomain>
2022-10-20 03:31:44 -0400

    Overall changes for added controller and model functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  # devise_for :users, controllers: { sessions: 'users/sessions' }
 
+  # Create default resource sets for games, challenges
+  resources :games
+  resources :challenges
 end

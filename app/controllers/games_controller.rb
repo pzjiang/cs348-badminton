d606474805da97184c12caa366c718fe9b4ae345
root <root@LAPTOP-2C7VJDRN.localdomain>
2022-10-20 01:27:26 -0400

    Added controller and model for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,41 @@
 class GamesController < ApplicationController
+    # Display all games
+    def index
+        @games = Game.all
+    end
+
+    # Enter data for a new game
+    def new
+        # Create a new instance of a game
+        @game = Game.new
+    end
+
+    # Create a new game (from games#new)
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            # On successful save operation, redirect to the new game's page
+            redirect_to @game
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    # Edit an existing game
+    def update
+    end
+
+    # Display a game
+    def show
+    end
+
+    # Delete an existing game
+    def destroy
+    end
+
+    # Enforce integrity of game input data
+    def game_params
+        params.require(:game).permit(:winner_id, :loser_id, :winner_score, :loser_score, :date, :location)
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
 class Game < ApplicationRecord
-    
+    # Ensure that the game is between two teams and that the loser has a lower score
+    validates :winner_id, presence: true
+    validates :loser_id, presence: true, comparison: {other_than: :winner_id}, message: "Losing team cannot be the same as winning team"
+
+    validates :winner_score, presence: true
+    validates :loser_score, presence: true, comparison: {less_than: :winner_score}, message: "Loser must have lower score than winner"
+
+    validates :date, presence: true
+    validates :location, presence: true
+
+    # The game is finished if it happens today or in the past
+    def finished?
+        :date <= DateTime.now
+    end
 end
